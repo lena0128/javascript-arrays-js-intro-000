84b464e20e50b368a187845cb4aad0232a9dc3b7
Lena Hou <lenahou2013@gmail.com>
2020-12-10 05:22:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function destructivelyAddElementToBeginningOfArray(array) {
-  var array =[];
+  var array =[1, 2, 3];
   return array.shift();
 }
