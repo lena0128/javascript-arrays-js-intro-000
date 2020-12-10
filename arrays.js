31737d22922b71e61063747b74af5bde8def0e7f
Lena Hou <lenahou2013@gmail.com>
2020-12-10 18:29:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  return array.index[index];
+  return array[index];
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
