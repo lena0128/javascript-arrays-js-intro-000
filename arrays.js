0352cd753aa8fce90fb0932aeda6b6bb89198077
Lena Hou <lenahou2013@gmail.com>
2020-12-10 05:33:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,8 +8,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var array = [];
-  var element = "";
   var newArray;
   newArray = [element, ...array];
   return newArray;
